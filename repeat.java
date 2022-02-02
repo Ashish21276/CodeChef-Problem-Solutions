356f787e32c83c5c1ee9056f84f945951cb34852
Ashish Kumar Jaiswal <66711249+Ashish21276@users.noreply.github.com>
2022-02-02 19:40:46 +0530

    Create repeat.java

---
+++ repeat.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.concurrent.SynchronousQueue;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(n * i);
+        }
+    }
+}
